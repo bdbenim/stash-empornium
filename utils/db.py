2553ec5123efbe210b9d2e5a2cd94210b440fea5
bdbenim <bdben@protonmail.com>
2023-12-05 18:28:58 -0800

    Update ORM with additional tracker tag maps

--- utils/db.py
+++ utils/db.py
@@ -54,6 +54,8 @@ def upgrade():
         con.commit()
 
 
+# alembic_version = db.Table("alembic_version", Column(name="version_num", type_=String(32)))
+
 emp_tag_map = db.Table(
     "emp_tags",
     Column("stashtag_id",
@@ -67,6 +69,30 @@ hf_tag_map = db.Table(
     Column("stashtag_id",
            ForeignKey("stash_tag.id", ondelete="CASCADE"), primary_key=True),
     Column("hftag_id",
+           ForeignKey("gazelle_tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+fc_tag_map = db.Table(
+    "fc_tags",
+    Column("stashtag_id",
+           ForeignKey("stash_tag.id", ondelete="CASCADE"), primary_key=True),
+    Column("fctag_id",
+           ForeignKey("gazelle_tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+pb_tag_map = db.Table(
+    "pb_tags",
+    Column("stashtag_id",
+           ForeignKey("stash_tag.id", ondelete="CASCADE"), primary_key=True),
+    Column("pbtag_id",
+           ForeignKey("gazelle_tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+ent_tag_map = db.Table(
+    "ent_tags",
+    Column("stashtag_id",
+           ForeignKey("stash_tag.id", ondelete="CASCADE"), primary_key=True),
+    Column("enttag_id",
            ForeignKey("gazelle_tags.id", ondelete="CASCADE"), primary_key=True)
 )
 
@@ -87,6 +113,15 @@ class GazelleTag(db.Model):
     hf_stash_tags: Mapped[list["StashTag"]] = db.relationship(secondary=hf_tag_map,
                                                               back_populates="hf_tags",
                                                               passive_deletes=True)  # type: ignore
+    fc_stash_tags: Mapped[list["StashTag"]] = db.relationship(secondary=fc_tag_map,
+                                                              back_populates="fc_tags",
+                                                              passive_deletes=True)  # type: ignore
+    pb_stash_tags: Mapped[list["StashTag"]] = db.relationship(secondary=pb_tag_map,
+                                                              back_populates="pb_tags",
+                                                              passive_deletes=True)  # type: ignore
+    ent_stash_tags: Mapped[list["StashTag"]] = db.relationship(secondary=ent_tag_map,
+                                                               back_populates="ent_tags",
+                                                               passive_deletes=True)  # type: ignore
 
 
 class StashTag(db.Model):
@@ -99,7 +134,17 @@ class StashTag(db.Model):
                                                          back_populates="emp_stash_tags",
                                                          passive_deletes=True)  # type: ignore
     hf_tags: Mapped[list[GazelleTag]] = db.relationship("GazelleTag", secondary=hf_tag_map,
-                                                        back_populates="hf_stash_tags")  # type: ignore
+                                                        back_populates="hf_stash_tags",
+                                                        passive_deletes=True)  # type: ignore
+    fc_tags: Mapped[list[GazelleTag]] = db.relationship("GazelleTag", secondary=fc_tag_map,
+                                                        back_populates="fc_stash_tags",
+                                                        passive_deletes=True)  # type: ignore
+    pb_tags: Mapped[list[GazelleTag]] = db.relationship("GazelleTag", secondary=pb_tag_map,
+                                                        back_populates="pb_stash_tags",
+                                                        passive_deletes=True)  # type: ignore
+    ent_tags: Mapped[list[GazelleTag]] = db.relationship("GazelleTag", secondary=ent_tag_map,
+                                                         back_populates="ent_stash_tags",
+                                                         passive_deletes=True)  # type: ignore
     categories: Mapped[list["Category"]] = db.relationship("Category", secondary=list_tags,
                                                            back_populates="tags",
                                                            passive_deletes=True)  # type: ignore
@@ -142,7 +187,7 @@ def get_or_create_no_commit[T](model: type[T], **kwargs) -> T:
 def to_dict() -> dict[str, Any]:
     categories: list[Category] = Category.query.all()
     s_tags: list[StashTag] = StashTag.query.all()
-    e_tags: list[GazelleTag] = GazelleTag.query.all()
+    g_tags: list[GazelleTag] = GazelleTag.query.all()
     data = {"revision": "", "stash_tags": [], "gazelle_tags": [], "categories": []}
     for tag in s_tags:
         stag = {
@@ -152,6 +197,9 @@ def to_dict() -> dict[str, Any]:
             "ignored": tag.ignored,
             "emp_tags": [e.id for e in tag.emp_tags],
             "hf_tags": [h.id for h in tag.hf_tags],
+            "fc_tags": [h.id for h in tag.fc_tags],
+            "pb_tags": [h.id for h in tag.pb_tags],
+            "ent_tags": [h.id for h in tag.ent_tags],
             "categories": [c.id for c in tag.categories],
         }
         data["stash_tags"].append(stag)
@@ -160,7 +208,7 @@ def to_dict() -> dict[str, Any]:
         category = {"id": cat.id, "name": cat.name}
         data["categories"].append(category)
 
-    for tag in e_tags:
+    for tag in g_tags:
         etag = {"id": tag.id, "name": tag.tagname}
         data["gazelle_tags"].append(etag)
 
@@ -175,37 +223,49 @@ def from_dict(data: dict[str, Any]) -> None:
     if data["revision"] != rev:
         raise ValueError("Schema version mismatch")
 
-    # 1. Delete existing records
-    Category.query.delete()
-    StashTag.query.delete()
-    GazelleTag.query.delete()
+    with db.session.begin():
+        # 1. Delete existing records
+        Category.query.delete()
+        StashTag.query.delete()
+        GazelleTag.query.delete()
 
-    # 2. Categories
-    for cat in data["categories"]:
-        db.session.add(Category(id=cat["id"], name=cat["name"]))  # type: ignore
-    db.session.flush()
+        # 2. Categories
+        for cat in data["categories"]:
+            db.session.add(Category(id=cat["id"], name=cat["name"]))  # type: ignore
+        db.session.flush()
 
-    # 3. EMP Tags
-    for tag in data["gazelle_tags"]:
-        db.session.add(GazelleTag(id=tag["id"], tagname=tag["name"]))  # type: ignore
-    db.session.flush()
+        # 3. EMP Tags
+        for tag in data["gazelle_tags"]:
+            db.session.add(GazelleTag(id=tag["id"], tagname=tag["name"]))  # type: ignore
+        db.session.flush()
 
-    # 4. Stash Tags
-    for tag in data["stash_tags"]:
-        stag = StashTag(
-            id=tag["id"], tagname=tag["name"], ignored=tag["ignored"], display=tag["display"]
-        )  # type: ignore
-        db.session.add(stag)
-        for id in tag["categories"]:
-            cat = Category.query.filter_by(id=id).first()
-            assert cat is not None
-            stag.categories.append(cat)
-        for id in tag["emp_tags"]:
-            etag = GazelleTag.query.filter_by(id=id).first()
-            assert etag is not None
-            stag.emp_tags.append(etag)
-        for id in tag["hf_tags"]:
-            etag = GazelleTag.query.filter_by(id=id).first()
-            assert etag is not None
-            stag.hf_tags.append(etag)
-    db.session.commit()
+        # 4. Stash Tags
+        for tag in data["stash_tags"]:
+            stag = StashTag(
+                id=tag["id"], tagname=tag["name"], ignored=tag["ignored"], display=tag["display"]
+            )  # type: ignore
+            db.session.add(stag)
+            for tag_id in tag["categories"]:
+                cat = Category.query.filter_by(id=tag_id).first()
+                assert cat is not None
+                stag.categories.append(cat)
+            for tag_id in tag["emp_tags"]:
+                etag = GazelleTag.query.filter_by(id=tag_id).first()
+                assert etag is not None
+                stag.emp_tags.append(etag)
+            for tag_id in tag["hf_tags"]:
+                etag = GazelleTag.query.filter_by(id=tag_id).first()
+                assert etag is not None
+                stag.hf_tags.append(etag)
+            for tag_id in tag["fc_tags"]:
+                etag = GazelleTag.query.filter_by(id=tag_id).first()
+                assert etag is not None
+                stag.fc_tags.append(etag)
+            for tag_id in tag["pb_tags"]:
+                etag = GazelleTag.query.filter_by(id=tag_id).first()
+                assert etag is not None
+                stag.pb_tags.append(etag)
+            for tag_id in tag["ent_tags"]:
+                etag = GazelleTag.query.filter_by(id=tag_id).first()
+                assert etag is not None
+                stag.ent_tags.append(etag)
