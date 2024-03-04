a53d1e300bfd1ba9e343fc63913e44cb17cbdd66
bdbenim <bdben@protonmail.com>
2024-03-03 17:03:55 -0800

    Add function to calculate directory size

--- utils/paths.py
+++ utils/paths.py
@@ -1,4 +1,7 @@
-def mapPath(path: str, pathmaps: dict[str,str]) -> str:
+import os
+
+
+def mapPath(path: str, pathmaps: dict[str, str]) -> str:
     # Apply remote path mappings
     for remote, local in pathmaps.items():
         if not path.startswith(remote):
@@ -10,3 +13,14 @@ def mapPath(path: str, pathmaps: dict[str,str]) -> str:
         path = local + path.removeprefix(remote)
         break
     return path
+
+
+def get_dir_size(path):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
